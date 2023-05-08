c526aa7e4ecabe2f1c200ee95f418e229f2bf33d
Fernando Pérez <perezmawyin@gmail.com>
2023-05-08 14:53:42 -0500

    Budget perez
    Lab

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,38 @@
+import React, { useContext } from "react"
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from "../context/AppContext"
 
+const ExpenseItem = (props) => {
+    const {dispatch} = useContext(AppContext)
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.name
+        })
+    }
+
+    const increaseAllocation = () => {
+        const expense = {
+            name: props.name,
+            cost: 10 /* Explicitly increase by 10 */
+        }
+
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        })
+
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>£{props.cost}</td>
+            <td><button onClick={event=> increaseAllocation()}>+</button></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    )
+}
+
+export default ExpenseItem
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,20 @@
 
+import { useContext } from "react"
+import { AppContext } from "../context/AppContext"
+
+const ExpenseTotal = () => {
+    const { expenses } = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost)
+    }, 0)
+
+    return (
+        <div className={`alert alert-primary`}>
+            <span>Expense Total: £{totalExpenses}</span>
+        </div>
+    )
+
+}
+
+export default ExpenseTotal
